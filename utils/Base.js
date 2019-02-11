c64717ceedee2d5d1fab8fe36ca464d32553adf8
sdttttt <760159537@qq.com>
2019-02-12 02:05:32 +0800

    2019-2-12

--- utils/Base.js
+++ utils/Base.js
@@ -1,11 +1,13 @@
 import { Config } from "Config.js";
+import { Token } from "Token.js";
 
 class Base{
   constructor(){
     this.url = Config.APIv1Url;
   }
 
-  request(params,callback){
+  request(params, callback, noRefetch){
+    var that = this;
     this.url += params.url;
     if(!params.method){
       params.method = "GET";
@@ -14,10 +16,31 @@ class Base{
       url: this.url,
       method:params.method,
       success:function(res){
-        callback && callback(res.data);
+          console.log(res);
+        var code = res.statusCode.toString();
+        var statusCode = code.charAt(0);
+        console.log(statusCode);
+        if(statusCode == '2'){
+          callback && callback(res.data);
+        }else if(code == "401"){
+          if(!noRefetch){
+            that._refetch(params,callback);
+          }
+        }
+      },
+      fail:function(err){
+        console.log(err);
       }
-    })
+    });
   }
+
+  _refetch(params,callback){
+    var token = new Token();
+    token.getTokenFromServer((res)=>{
+      this.request(params,callback,true);
+    });
+  }
+  
 }
 
 export { Base }; 
---
+++ utils/Token.js
@@ -0,0 +1,52 @@
+import { Config } from "Config.js";
+
+class Token{
+  
+  constructor(){
+    this.loginUrl = Config.APIv1Url + "login";
+    this.checkUrl = Config.APIv1Url + "verify";
+  }
+
+  verify(){
+    var token = wx.getStorageSync('token');
+    if(!token){
+      this.getTokenFromServer();
+    }else{
+      this._verifyFromServer(token);
+    }
+  }  
+
+  _verifyFromServer(token){
+    var that = this;
+    wx.request({
+      url: that.checkUrl,
+      method:"POST",
+      data:{ token:token },
+      success:function(res){
+        var status = res.data.isVerify;
+        if(!isVerify){
+          that.getTokenFromServer();
+        }
+      }
+    })
+  }
+
+  getTokenFromServer(callback){
+    var that = this;
+    wx.login({
+      success:function(res){
+        wx.request({
+          url: that.loginUrl,
+          method:"POST",
+          data:{ code:res.code },
+          success:function(res){
+            wx.setStorageSync("token",res.data.token);
+            callback && callback(res.data.token);
+          }
+        });
+      }
+    });
+  }
+}
+
+export { Token };
